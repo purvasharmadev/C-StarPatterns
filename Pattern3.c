0091075ba6e372dfcaff685b2185068dfeecaf8a
Purva <purva08815294379@gmail.com>
2020-10-12 17:34:11 +0530

    StarPattern

---
+++ Pattern3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j;
+    for(i=0;i<=5;i++)
+    {
+        for(j=0;j<=5;j++)
+        {
+            if(j>=6-i)
+            {
+                printf("*");
+            }
+            else{
+                printf(" ");
+                }
+        }
+            printf("\n");
+
+    }
+    return 0;
+}
+//OUTPUT:
+/*
+
+
+    *
+   **
+  ***
+ ****
+*****
+
+
+*/
---
+++ pattern4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if(j<=6-i)
+            {
+                printf("*");
+            }
+            else{
+                printf(" ");
+                }
+        }
+            printf("\n");
+
+    }
+    return 0;
+}
+
+//OUTPUT
+/*
+
+*****
+****
+***
+**
+*
+
+
+*/
+
